801c5c441e55c534c0c8cead39cecb6475b6f49d
Michal Voscek <michal.voscek@gmail.com>
2022-11-26 10:40:41 +0100

    Add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,15 @@ import {AppContext} from '../contexts/AppContext'
 export default function Home() {
   const {lists} = useContext(AppContext)!
   return (
-    <div>
-      <Link href="/todolist">List</Link>
-      List of Todo lists
-      {JSON.stringify(lists)}
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        <Link href="/todolist">List</Link>
+        List of Todo lists
+        {JSON.stringify(lists)}
+      </div>
+    </>
   )
 }
