a4e4c83aa805c49f3fa856efa82e0f5a0beb6d33
Michal Voscek <michal.voscek@gmail.com>
2022-11-28 09:16:57 +0100

    Add active/finished filter

--- pages/todolist.tsx
+++ pages/todolist.tsx
@@ -1,15 +1,24 @@
 import {useRouter} from 'next/router'
-import {useContext} from 'react'
+import {useContext, useMemo} from 'react'
 import {AppContext} from '../contexts/AppContext'
 import {List, Item} from '../types'
 import {AddItem} from '../components/AddItem'
 import {markFinished, deleteItem} from '../utlls/comm'
 
-export default function Home() {
+export default function ToDoList() {
   const router = useRouter()
   const listId: string = router.query.id as string
-  const {lists, fetchData} = useContext(AppContext)!
+  const {lists, fetchData, filter, setFilter} = useContext(AppContext)!
   const list: List = lists[listId]
+  const items: Item[] = useMemo(() => {
+    if (!list) return []
+    return list.items.filter((item: Item) => {
+      if (filter === 'all') return true
+      if (filter === 'active' && !item.finished) return true
+      if (filter === 'finished' && item.finished) return true
+      return false
+    })
+  }, [lists, filter, listId])
   if (!list) return (<div>Loading...</div>)
   const onFinishedClicked = (itemId: string) => async () => {
     await markFinished(listId, itemId)
@@ -19,13 +28,24 @@ export default function Home() {
     await deleteItem(listId, itemId)
     await fetchData()
   }
+  const onFilterChange = (event: any) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div className="grid grid-rows-18 max-h-screen px-4">
-      <h1 className="text-3xl font-bold underline row-span-1">
-        {list.title}
-      </h1>
+      <div>
+        <h1 className="text-3xl font-bold underline row-span-1">
+          {list.title}
+        </h1>
+        <select className="select select-bordered w-full max-w-xs" value={filter} onChange={onFilterChange}>
+          <option value="all">All</option>
+          <option value="active">Active</option>
+          <option value="finished">Finished</option>
+        </select>
+      </div>
       <div className="overflow-y-auto row-span-15">
-      {list.items.map((item: Item) => (
+      {items.map((item: Item) => (
         <div key={item.id} className="card w-96 bg-base-100 shadow-xl">
           <div className="card-body">
             <h2 className="card-title">{item.title}</h2>
