0199e2d2a7c28a0123fbe4dac4a387947cf6e93a
Michal Voscek <michal.voscek@gmail.com>
2022-11-30 12:05:22 +0100

    Remove unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,7 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {
-      gridTemplateRows: {
-        '16': 'repeat(16, minmax(0, 1fr))',
-        '18': 'repeat(18, minmax(0, 1fr))',
-      },
-      gridRow: {
-        'span-12': 'span 12 / span 12',
-        'span-13': 'span 13 / span 13',
-        'span-14': 'span 14 / span 14',
-        'span-15': 'span 15 / span 15',
-      }
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
 }
